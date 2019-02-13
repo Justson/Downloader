88caf66d311daed5677e22f4318aecb0b2b82681
Justson <xiaozhongcen@gmail.com>
2019-02-14 07:56:39 +0800

    1.update download listener

--- library/src/main/java/com/downloader/library/DownloadListener.java
+++ library/src/main/java/com/downloader/library/DownloadListener.java
@@ -29,8 +29,8 @@ public interface DownloadListener {
 
 	/**
 	 * @param url                下载链接
-	 * @param userAgent          mUserAgent
-	 * @param contentDisposition mContentDisposition
+	 * @param userAgent          UserAgent
+	 * @param contentDisposition ContentDisposition
 	 * @param mimetype           资源的媒体类型
 	 * @param contentLength      文件长度
 	 * @param extra              下载配置 ，可以通过 Extra 修改下载icon ， 关闭进度条 ， 或者是否强制下载。
