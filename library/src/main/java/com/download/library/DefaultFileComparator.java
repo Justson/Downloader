df7072d01f6b2ad33a181b1eff6529fb88ae988e
Justson <xiaozhongcen@gmail.com>
2019-08-09 02:10:11 +0800

    1.add FileComparator

---
+++ library/src/main/java/com/download/library/DefaultFileComparator.java
@@ -0,0 +1,9 @@
+package com.download.library;
+
+/**
+ * @author cenxiaozhong
+ * @date 2019-08-09
+ * @since 1.0.0
+ */
+public class DefaultFileComparator {
+}
---
+++ library/src/main/java/com/download/library/FileComparator.java
@@ -0,0 +1,18 @@
+package com.download.library;
+
+import android.support.annotation.NonNull;
+
+import java.io.File;
+import java.util.Comparator;
+
+/**
+ * @author cenxiaozhong
+ * @date 2019-08-09
+ * @since 1.0.0
+ */
+public class FileComprare implements Comparator<File> {
+	@Override
+	public int compare(File o1, File o2) {
+		return 0;
+	}
+}
